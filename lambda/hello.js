64bf7bd4d439475877c414cd32d41e290069bd2a
binums <binums56@gmail.com>
2021-05-27 02:36:37 +0530

    staged

--- lambda/hello.js
+++ lambda/hello.js
@@ -7,6 +7,6 @@ exports.handler = async (event) => {
 	return {
 		statusCode: 200,
 		headers: { "Content-Type": "text/plain" },
-		body: `Hello, CDK! You've hit ${event.path}\n` 
+		body: `Hello, CDK! (staged) You've hit ${event.path}\n` 
 	};
 };
