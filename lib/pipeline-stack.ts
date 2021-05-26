9aadd267d76563666ae437f0d7c08dd39ccf7b33
binums <binums56@gmail.com>
2021-05-27 00:26:11 +0530

    added initial cdk pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from "@aws-cdk/core";
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
+import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+import { SecretValue } from "@aws-cdk/core";
 
 export interface WorkshopPipelineStackProps {}
 
@@ -9,5 +13,30 @@ export class WorkshopPipelineStack extends cdk.Construct {
 		props?: WorkshopPipelineStackProps
 	) {
 		super(scope, id);
+
+		const sourceArtifact = new codepipeline.Artifact();
+		const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+		new CdkPipeline(this, "Pipeline", {
+			pipelineName: "WorkshopPipeline",
+			cloudAssemblyArtifact,
+
+			sourceAction: new codepipeline_actions.GitHubSourceAction({
+				actionName: "GitHub",
+				output: sourceArtifact,
+				oauthToken: SecretValue.secretsManager(
+					"CDK-Pipeline-test-github-token"
+				),
+				branch: "main",
+				owner: "binums",
+				repo: "cdk-pipeline-test"
+			}),
+
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        buildCommand: "npm run build"
+      })
+		});
 	}
 }
